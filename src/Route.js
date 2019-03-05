93404897d04b0b207f747fe5cacb976fee1984c4
lorne <2270333671@qq.com>
2019-03-05 14:55:32 +0800

    现金桌H5

--- src/Route.js
+++ src/Route.js
@@ -11,7 +11,7 @@ import HomePage from "./pages/HomePage";
 const Routes = () => (
     <Router>
         <App>
-            <Route path="/" component={HomePage}/>
+            <Route path="/home" component={HomePage}/>
             <Route path="/loadApp/" component={LoadApp}/>
             <Route path="/infos/:id/" component={InfoDetail}/>
             <Route path="/main_events/:event_id/infos/:id/" component={EventDetail}/>
