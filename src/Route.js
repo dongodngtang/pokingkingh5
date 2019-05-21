feac9889b53a9a1125457f7478a04b1c439a43f6
lorne <2270333671@qq.com>
2019-05-21 15:29:49 +0800

    233

--- src/Route.js
+++ src/Route.js
@@ -14,10 +14,10 @@ import DemoHome from './pages/DemoHome'
 const Routes = () => (
     <Router>
         <App>
-            <Route path="/demo" component={DemoHome}/>
+            {/*<Route path="/demo" component={DemoHome}/>*/}
             <Route path="/queue_new" component={QueueListNewPage}/>
-            <Route path="/queue" component={QueueListPage}/>
-            <Route path="/home" component={HomePage}/>
+            {/*<Route path="/queue" component={QueueListPage}/>*/}
+            {/*<Route path="/home" component={HomePage}/>*/}
             <Route path="/loadApp/" component={LoadApp}/>
             <Route path="/infos/:id/" component={InfoDetail}/>
             <Route path="/main_events/:event_id/infos/:id/" component={EventDetail}/>
