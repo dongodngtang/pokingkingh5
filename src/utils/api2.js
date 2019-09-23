61fd5c9c9e150a456640799efcb9b26043d9534f
lorne <2270333671@qq.com>
2019-09-23 15:36:00 +0800

    马尼拉的h5 不做high_limit的限制 为了避免澳门再次修改回去，之前的代码放在了QueueListPage里面

--- src/utils/api2.js
+++ src/utils/api2.js
@@ -2,11 +2,11 @@ import {logMsg} from "./utils";
 
 const api2 = {
     //内部测试
-    dev: 'http://192.168.2.10:3000/v2/',
+    dev: 'http://192.168.2.10:3000/v1/',
     //test分支用来发布版本  test_ci_at用来跑自动化测试
-    test: 'http://test.pokerking_api.deshpro.com/v2',
+    test: 'http://test.pokerking_api.deshpro.com/v1',
     //production 用来发布正式生产环境
-    production: 'http://api.pokerkinglive.com/v2',
+    production: 'http://api.pokerkinglive.com/v1',
     cash_queues:cash_queues,//现金桌排队进程列表
 
 }
