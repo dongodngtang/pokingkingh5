5360b416158e43d519f8466d9c5fbe4da06124fa
lornem <ailuo8899@gmail.com>
2019-11-23 10:42:39 +0800

    发布

--- src/Route.js
+++ src/Route.js
@@ -15,15 +15,15 @@ import KingsApp from './pages/KingsApp'
 const Routes = () => (
     <Router>
         <App>
-            <Route path="/" component={QueueListNewPage}/>
+            
             <Route path="/queue_new" component={QueueListNewPage}/>
 
             <Route path="/manila" component={QueueMaNiLa}/>
 
             {/*<Route path="/queue" component={QueueListPage}/>*/}
             {/*<Route path="/home" component={HomePage}/>*/}
-            <Route path="/loadApp/" component={LoadApp}/>
-            <Route path="/kings_test/" component={KingsApp}/>
+            <Route path="/loadApp" component={LoadApp}/>
+            <Route path="/kings_test" component={KingsApp}/>
             <Route path="/infos/:id/" component={InfoDetail}/>
             <Route path="/main_events/:event_id/infos/:id/" component={EventDetail}/>
         </App>
